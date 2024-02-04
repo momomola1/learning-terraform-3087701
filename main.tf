1bf271bc2e72bf32bf33de84f0bf7e3cb2e857d8
momomola1 <58834999+momomola1@users.noreply.github.com>
2024-02-04 23:01:39 +0800

    create dev vpc

--- main.tf
+++ main.tf
@@ -18,7 +18,35 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id = module.blog_vpc.public_subnets[0]
   tags = {
     Name = "HelloWorld"
+  }
+}
+
+module "blog_sg" {
+  source = "terraform-aws-modules/security-groups/aws"
+  version = "4.13.0"
+  vpc_id = module.blog_vpc.vpc_id
+  name = "blog_vpc"
+  ingress_rules = ["https-443-tcp", "http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
