9e2e18edcda82280cc75fd43813f37068c440ef6
momomola1 <58834999+momomola1@users.noreply.github.com>
2024-02-05 21:01:55 +0800

    load balancer

--- main.tf
+++ main.tf
@@ -55,7 +55,8 @@ module "blog_vpc" {
 module "blog-alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name    = "my-alb"
+  name    = "blog-alb"
+  load_balancer_type = "application"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
@@ -103,12 +104,7 @@ module "blog-alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
+
     }
   ]
 
